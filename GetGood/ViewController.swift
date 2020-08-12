97d4d83ab47a4b781a9d049564daaead332f406c
Hiago Chagas <hiago.chagas.14@gmail.com>
2020-08-12 08:34:16 -0300

    Initial Commit

---
+++ GetGood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetGood
+//
+//  Created by Hiago Chagas on 12/08/20.
+//  Copyright © 2020 Hiago Chagas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
